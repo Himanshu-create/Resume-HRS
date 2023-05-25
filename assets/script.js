3c91b21df50eb80cfc76829f861746899c78edd6
Himanshu <hs092001@gmail.com>
2023-05-25 09:55:03 +0530

    Add project files.

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+// JavaScript source code
+function epm(){
+    document.getElementById("epm").scrollIntoView();
+}
+
+function skills(){
+    document.getElementById("skills").scrollIntoView();
+}
+
+function edu(){
+    document.getElementById("edu").scrollIntoView();
+}
+
+function hob(){
+    document.getElementById("hob").scrollIntoView();
+}
+
+function con(){
+    document.getElementById("contact").scrollIntoView();
+}
